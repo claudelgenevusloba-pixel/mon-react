0ac13e91f07cc87bfe8320cbb314b1d812089ee7
barroUser <barrodev@groupegain.com>
2026-09-24 12:53:00 +0000

    Troisième commit

---
+++ src/pages/page404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page404 = () => {
+  return (
+    <div>
+        <h1>page404</h1>
+        <p>Désolé, la page que vous recherchez n'existe pas.</p>
+    </div>
+  )
+}
+
+export default page404
